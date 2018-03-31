ad909ee4337d32ca0432c06dbd70c364d4c007a2
Diego Araujo <diegoishigh@gmail.com>
2018-03-31 15:07:54 -0300

    Apply fix from phpcs

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     public $timestamps = false;
-    protected $fillable = ['name', 'email', 'password'];
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
 }
